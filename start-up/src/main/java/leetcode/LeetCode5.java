f65dc497b943f23e914920def17b50b947279fb0
kami <zh_guomail@163.com>
2019-12-12 16:57:24 +0800

    LeetCode88-Merge Sorted Array

--- start-up/src/main/java/leetcode/LeetCode5.java
+++ start-up/src/main/java/leetcode/LeetCode5.java
@@ -478,12 +478,29 @@ public class LeetCode5 {
         return false;
     }
 
-
+    /**
+     * @description: 88. Merge Sorted Array
+     * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+     * Note:
+     * The number of elements initialized in nums1 and nums2 are m and n respectively.
+     * You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+     * Input:
+     * nums1 = [1,2,3,0,0,0], m = 3
+     * nums2 = [2,5,6],       n = 3
+     * Output: [1,2,2,3,5,6]
+     * 从后往前比较，大数先放置,依次往前走；
+     * 分析：因为两个数组合并后的有效数字的个数是确定的，所以可以从后往前一次排
+     * @author: kami
+     * @time: 2019/12/12 13:34
+     */   
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        while(n>0) nums1[m+n-1] = (m==0||nums2[n-1] > nums1[m-1]) ? nums2[--n] : nums1[--m];
+        for (int a:nums1) {
+            System.out.println(a);
+        }
+    }
 
     public static void main(String[] args) {
-        String s = "great";
-        String t = "rgate";
-        System.out.println(isScramble(s,t));
 
     }
 }
