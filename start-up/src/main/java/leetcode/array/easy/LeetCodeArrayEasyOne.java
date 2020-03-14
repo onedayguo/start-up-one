a1f239c87becfe0149fb31283b30f5155489199f
kami <zh_guomail@163.com>
2020-03-14 21:41:57 +0800

    697. Degree of an Array，完成

--- start-up/src/main/java/leetcode/array/easy/LeetCodeArrayEasyOne.java
+++ start-up/src/main/java/leetcode/array/easy/LeetCodeArrayEasyOne.java
@@ -1,5 +1,7 @@
 package leetcode.array.easy;
 
+import leetcode.array.LeetCodeArrayEasy;
+
 import java.util.*;
 
 /**
@@ -9,15 +11,23 @@ import java.util.*;
  * @Version: 1.0.0
  */
 public class LeetCodeArrayEasyOne {
+
+    public static void main(String[] args) {
+        int[] array = {1,2,2,3,1};
+        System.out.println(findShortestSubArray(array));
+
+    }
     /**
      * @description: 697. Degree of an Array
      * Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements.
      * Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
+     * 思路：一：遍历数组，将元素作为key,将元素的数量，开始左指针，结束右指针作为value，键值对放入map
+     *      二：遍历map,找出其中元素数量最多，右指针-左指针值最小的那个
      * @return: smallest length of subarray
      * @auther: kami
      * @date: 2020/3/8 21:15
      */
-    public int findShortestSubArray(int[] nums) {
+    public static int findShortestSubArray(int[] nums) {
         int length = nums.length;
         Map<Integer, Map<Integer,Integer>> count = new HashMap<>();//0数量，1 left,2 right
 
@@ -35,12 +45,17 @@ public class LeetCodeArrayEasyOne {
             }
             count.put(nums[i],map);
         }
-        int max = 0,len = 0,res = 0;
-        for (Map<Integer,Integer> map: count.values()) {
-            if (map.get(0) == max) res = Math.min(map.get(2)-map.get(1),len);
-            max = Math.max(map.get(0),max);
-            len = Math.max(map.get(2)-map.get(1),len);
+        int max = 0;
+        for (Map<Integer,Integer> map: count.values()){
+            if (map.get(0) >= max ){
+                if (map.get(0) > max){
+                    max = map.get(0);
+                    length = map.get(2)-map.get(1);
+                }else {
+                    length = Math.min(map.get(2)-map.get(1),length);
+                }
+            }
         }
-        return res;
+        return length+1;
     }
 }
