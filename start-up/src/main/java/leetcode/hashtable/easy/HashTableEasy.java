68598bee14a02b7bd306b8a0788ce68f3b4534e1
kami <zh_guomail@163.com>
2020-03-19 23:59:18 +0800

    961. N-Repeated Element in Size 2N Array

--- start-up/src/main/java/leetcode/hashtable/easy/HashTableEasy.java
+++ start-up/src/main/java/leetcode/hashtable/easy/HashTableEasy.java
@@ -43,4 +43,23 @@ public class HashTableEasy {
         }
         return cnt;
     }
+
+    /**
+     * @description: 961. N-Repeated Element in Size 2N Array
+     * In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times
+     * Return the element repeated N times.
+     * @return: 
+     * @auther: kami
+     * @date: 2020/3/19 23:48
+     */
+    public int repeatedNTimes(int[] A) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < A.length; i++) {
+            if (set.contains(A[i])){
+                return A[i];
+            }
+            set.add(A[i]);
+        }
+        return A[0];
+    }
 }
