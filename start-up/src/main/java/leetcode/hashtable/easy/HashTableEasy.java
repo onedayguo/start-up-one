3b3e2c06764e5412e11af52fb8ee56f62efae5a0
kami <zh_guomail@163.com>
2020-03-19 08:25:52 +0800

    771. Jewels and Stones

---
+++ start-up/src/main/java/leetcode/hashtable/easy/HashTableEasy.java
@@ -0,0 +1,25 @@
+package leetcode.hashtable.easy;
+
+/**
+ * @Description: hashtable简单级别的LeetCode题目
+ * @Auther: kami
+ * @Date: 2020/3/19 08:24
+ * @Version: 1.0.0
+ * @Company: 威富通科技有限公司
+ * @Copyright: SwiftPass Technologies Co., LTD. Rights Reserved
+ */
+public class HashTableEasy {
+    /**
+     * @description: 771. Jewels and Stones
+     * You're given strings J representing the types of stones that are jewels, and S representing the stones you have.
+     * Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+     * The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive,
+     * so "a" is considered a different type of stone from "A".
+     * @return:
+     * @auther: kami
+     * @date: 2020/3/19 8:24
+     */
+    public int numJewelsInStones(String J, String S) {
+        return 0;
+    }
+}
