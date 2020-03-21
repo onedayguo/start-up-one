45c848fd94f86d19800170a4640396b1fdd3746c
kami <zh_guomail@163.com>
2020-03-21 18:28:21 +0800

    1025. Divisor Game

--- start-up/src/main/java/leetcode/dynamic/DynamicProgrammingEasy.java
+++ start-up/src/main/java/leetcode/dynamic/DynamicProgrammingEasy.java
@@ -1,10 +1,25 @@
 package leetcode.dynamic;
 
 /**
- * @Description: TODO
+ * @Description: 动态规划
  * @Auther: kami
  * @Date: 2020/3/21 10:33
  * @Version: 1.0.0
  */
 public class DynamicProgrammingEasy {
+    /**
+     * @description: 1025. Divisor Game
+     * Alice and Bob take turns playing a game, with Alice starting first.
+     * Initially, there is a number N on the chalkboard.  On each player's turn, that player makes a move consisting of:
+     * Choosing any x with 0 < x < N and N % x == 0.
+     * Replacing the number N on the chalkboard with N - x.
+     * Also, if a player cannot make a move, they lose the game.
+     * Return True if and only if Alice wins the game, assuming both players play optimally.
+     * @return: alice是否赢得游戏
+     * @auther: kami
+     * @date: 2020/3/21 10:35
+     */
+    public boolean divisorGame(int N) {
+        return  N % 2 == 0;
+    }
 }
