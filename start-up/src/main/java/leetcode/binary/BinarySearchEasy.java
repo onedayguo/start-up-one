66d34631bfbca3563507b73676dc7155f6413f4b
kami <zh_guomail@163.com>
2020-03-21 10:33:06 +0800

    1207. Unique Number of Occurrences

--- start-up/src/main/java/leetcode/binary/BinarySearchEasy.java
+++ start-up/src/main/java/leetcode/binary/BinarySearchEasy.java
@@ -1,5 +1,9 @@
 package leetcode.binary;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @Description: binary search简单级别的LeetCode题目
  * @Auther: kami
@@ -13,12 +17,32 @@ public class BinarySearchEasy {
      * @description: 1207. Unique Number of Occurrences
      * Given an array of integers arr, write a function that returns true if and only if the number of
      * occurrences of each value in the array is unique.
-     * @return: 
+     * @return: 是否每个元素出现次数都是唯一的
      * @auther: kami
      * @date: 2020/3/20 0:05
      */
-    public boolean uniqueOccurrences(int[] arr) {
-        return false;
+    public static boolean uniqueOccurrences(int[] arr) {
+        Arrays.sort(arr);
+        Set<Integer> set = new HashSet<>();
+        int count = 0;
+        for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i] != arr[i+1]){
+                count++;
+                if (set.contains(count)) return false;
+                else {
+                    set.add(count);
+                    count = 0;
+                }
+            }else {
+                count++;
+            }
+        }
+        return set.contains(count+1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,1,1,3};
+        boolean mmm = uniqueOccurrences(arr);
     }
 
     /**
@@ -31,13 +55,14 @@ public class BinarySearchEasy {
      * @auther: kami
      * @date: 2020/3/21 9:00
      */
+    //基础版
     public boolean isBalanced(TreeNode root) {
         if (root == null) return true;
         int left = dfs(root.left);
         int right = dfs(root.right);
         return Math.abs(left-right)<=1 && isBalanced(root.left) && isBalanced(root.right);
     }
-    public int dfs(TreeNode node){
+    public int dfs(TreeNode node)   {
         if (node == null) return 0;
         //利用dfs实现子树深度的求取 ，父节点的depth为左右儿子节点max值+1。
         int left = dfs(node.left) + 1;
