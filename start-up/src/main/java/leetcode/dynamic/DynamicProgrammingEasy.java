73e8da814a520a38a083676cd704dbe766be2b62
kami <zh_guomail@163.com>
2020-03-21 20:40:37 +0800

    365. Water and Jug Problem 水壶问题

--- start-up/src/main/java/leetcode/dynamic/DynamicProgrammingEasy.java
+++ start-up/src/main/java/leetcode/dynamic/DynamicProgrammingEasy.java
@@ -22,4 +22,34 @@ public class DynamicProgrammingEasy {
     public boolean divisorGame(int N) {
         return  N % 2 == 0;
     }
+
+    /**
+     * @description: 121. Best Time to Buy and Sell Stock
+     * Say you have an array for which the ith element is the price of a given stock on day i.
+     * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock),
+     * design an algorithm to find the maximum profit
+     * Note that you cannot sell a stock before you buy one.
+     * @return: 获取最大利益
+     * @auther: kami
+     * @date: 2020/3/21 18:29
+     */
+    public static int maxProfit(int[] prices) {
+        int lastIndex = prices.length-1;
+        int max = prices[0],min = prices[lastIndex];
+        int priceDif = max - min;
+        for (int i = 0,j = lastIndex; i < prices.length && j > 0;i++,j--) {
+            if (prices[i] > max){
+                max = prices[i];
+            }
+            if (prices[j] < min){
+                min = prices[j--];
+            }
+        }
+        return max-min;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7,1,5,3,6,4};
+        int am = maxProfit(arr);
+    }
 }
