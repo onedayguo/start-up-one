110d0cea388199307ecb4631846b27ee0c59cbc2
kami <zh_guomail@163.com>
2020-03-22 19:51:23 +0800

    1277. Count Square Submatrices with All Ones

--- start-up/src/main/java/leetcode/dynamic/DynamicProgrammingEasy.java
+++ start-up/src/main/java/leetcode/dynamic/DynamicProgrammingEasy.java
@@ -160,7 +160,31 @@ public class DynamicProgrammingEasy {
             }
         }
         return matrixBlockSum;
-    }   
+    }
+
+    /**
+     * @description: 1277. Count Square Submatrices with All Ones
+     * Given a m * n matrix of ones and zeros, return how many square submatrices have all ones.
+     * 思路：这是一道动态规划题
+     *      1.计算以当前点为右下角的方块的数量
+     *      2.累加每个点左，上，左上点方块数量
+     * @return: 数字是1的方块数量
+     * @auther: kami
+     * @date: 2020/3/22 17:46
+     */
+    public int countSquares(int[][] matrix) {
+        int count = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] > 0 && i > 0 && j > 0){
+                    // 当前点数量 = 左，上，左上最中最小值 +1，最后+1是因为当前点值为1
+                    matrix[i][j] = Math.min(matrix[i-1][j],Math.min(matrix[i][j-1],matrix[i-1][j-1])) + 1;
+                }
+                count += matrix[i][j];
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 1};
         int am = rob(arr);
