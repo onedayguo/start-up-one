527877c32023d9016a6f1144574b6c8a4d532787
kami <zh_guomail@163.com>
2020-03-22 19:33:02 +0800

    88. Merge Sorted Array

--- start-up/src/main/java/leetcode/together/Leetcode.java
+++ start-up/src/main/java/leetcode/together/Leetcode.java
@@ -32,7 +32,34 @@ public class Leetcode {
         return gcd(b,a%b);
     }
 
+    /**
+     * @description: 88. Merge Sorted Array
+     * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+     * The number of elements initialized in nums1 and nums2 are m and n respectively.
+     * You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+     * @return:
+     * @auther: kami
+     * @date: 2020/3/22 19:19
+     */
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int temp = m + n - 1;
+        m--;
+        n--;
+        while (n >= 0){
+            if (nums1[m] <= nums2[n]){
+                nums1[temp] = nums2[n];
+                n--;
+            }else {
+                nums1[temp] = nums1[m];
+                m--;
+            }
+            temp--;
+        }
+    }
+
     public static void main(String[] args) {
-        canMeasureWater(34,4,6);
+        int[] nums1 = {0};
+        int[] nums2 = {1};
+        merge(nums1,1,nums2,1);
     }
 }
