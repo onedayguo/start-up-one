33b561483235270a274949c11c73a8bd4978662a
kami <zh_guomail@163.com>
2020-03-22 17:01:21 +0800

    392. Is Subsequence

--- start-up/src/main/java/leetcode/dynamic/DynamicProgrammingEasy.java
+++ start-up/src/main/java/leetcode/dynamic/DynamicProgrammingEasy.java
@@ -108,6 +108,30 @@ public class DynamicProgrammingEasy {
         }
         return currMin;
     }
+
+    /**
+     * @description: 392. Is Subsequence
+     * Given a string s and a string t, check if s is subsequence of t.
+     * You may assume that there is only lower case English letters in both s and t. t is potentially a very long (length ~= 500,000)
+     * string, and s is a short string (<=100).
+     * A subsequence of a string is a new string which is formed from the original string by deleting some (can be none)
+     * of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence
+     * of "abcde" while "aec" is not).
+     * @return: 是否是子字符串，不管是不是连续子字符串
+     * @auther: kami
+     * @date: 2020/3/22 16:45
+     */
+    public boolean isSubsequence(String s, String t) {
+        int tIndex = 0;
+        for (char currChar:s.toCharArray()) {
+            int offSet = t.substring(tIndex).indexOf(currChar);
+            tIndex = tIndex + offSet + 1;
+            if (offSet < 0){
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 1};
         int am = rob(arr);
