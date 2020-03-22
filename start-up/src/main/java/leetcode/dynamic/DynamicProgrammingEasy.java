4e7eae1abd326ea238cc9a6eb320b6c363d7fdb9
kami <zh_guomail@163.com>
2020-03-22 16:45:03 +0800

    746. Min Cost Climbing Stairs

--- start-up/src/main/java/leetcode/dynamic/DynamicProgrammingEasy.java
+++ start-up/src/main/java/leetcode/dynamic/DynamicProgrammingEasy.java
@@ -84,12 +84,29 @@ public class DynamicProgrammingEasy {
 
     /**
      * @description: 746. Min Cost Climbing Stairs
-     * @return:
+     * On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+     * Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top
+     * of the floor, and you can either start from the step with index 0, or the step with index 1.
+     * cost will have a length in the range [2, 1000].
+     * Every cost[i] will be an integer in the range [0, 999].
+     * 如果我们用一个数组dp[]来存放到达每一层所需要的花费值。则则最终结果是求dp[cost.length]的值。因为每次可以走1层或者2层，
+     * 并且可以从0或者从1开始，所以可以得到dp[0]为0，dp[1]为0。从2开始，dp[i]可以由通过dp[i-2]走2层或者通过dp[i-1]走一层到达，
+     * 而这i-2和i-1层所要花费的值分别为cost[i-2]和cost[i-1]，所以，dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1])。
+     * 该算法的空间复杂度为O(n)，时间复杂度为O(n)。
+     * @return: 到楼梯最高阶的最小花费
      * @auther: kami
      * @date: 2020/3/22 16:17
      */
     public int minCostClimbingStairs(int[] cost) {
-
+        int pre1 = 0;
+        int pre2 = 0;
+        int currMin = 0;
+        for (int i = 2; i < cost.length+1; i++) {
+            currMin = Math.min(pre1+cost[i-1],pre2+cost[i-2]);
+            pre2 = pre1;
+            pre1 = currMin;
+        }
+        return currMin;
     }
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 1};
