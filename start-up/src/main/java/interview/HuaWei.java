91493ed29f53d9b341680c078a5bc15aef9f2ce1
kami <zh_guomail@163.com>
2020-03-29 17:52:28 +0800

    牛客-解码

--- start-up/src/main/java/interview/HuaWei.java
+++ start-up/src/main/java/interview/HuaWei.java
@@ -2,6 +2,7 @@ package interview;
 
 import com.sun.deploy.util.StringUtils;
 
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -126,6 +127,28 @@ public class HuaWei {
         }
         return count;
     }
+
+    /**
+     * @description: 牛客-解码
+     * 小Q想要给他的朋友发送一个神秘字符串，但是他发现字符串的过于长了，于是小Q发明了一种压缩算法对字符串中重复的部分进行了压缩，
+     *对于字符串中连续的m个相同字符串S将会压缩为[m|S](m为一个整数且1<=m<=100)，例如字符串ABCABCABC将会被压缩为[3|ABC]，
+     * 现在小Q的同学收到了小Q发送过来的字符串，你能帮助他进行解压缩么？
+     * @return:
+     * @auther: kami
+     * @date: 2020/3/29 15:15
+     */
+    public static String decode(String words){
+        while (words.contains("]")){
+            int right = words.indexOf("]");
+            int left = words.lastIndexOf("[", right);
+            String repeatStr = words.substring(left+1, right);
+            String[] split = repeatStr.split("\\|");
+            words = words.replace("["+repeatStr+"]",
+                    String.join("", Collections.nCopies(Integer.parseInt(split[0]), split[1])));
+        }
+        return words;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         while (in.hasNext()){
