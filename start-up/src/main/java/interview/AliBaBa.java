c203922a0ca920e203419a471aca813e511f55a1
kami <zh_guomail@163.com>
2020-03-30 20:01:56 +0800

    阿里笔试题

--- start-up/src/main/java/interview/AliBaBa.java
+++ start-up/src/main/java/interview/AliBaBa.java
@@ -1,4 +1,6 @@
 package interview;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 /**
  * @Description: 阿里巴巴
@@ -9,14 +11,90 @@ import java.util.Scanner;
 public class AliBaBa {
 
 
-    static int mod = 1000000007;
-
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        System.out.println((pow(n - 1) * n) % mod);
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            int nmk = Integer.parseInt(in.nextLine());
+            String nums = in.nextLine();
+            maxSub(nmk,nums);
+        }
     }
-    
+
+
+    /**
+     * @description: 最大子序列期望值
+     * @return:
+     * @auther: kami
+     * @date: 2020/3/30 19:38
+     */
+    public static void maxSub(int n,String nums){
+        String[] numsStr = nums.split(" ");
+        int[] numInt = new int[n];
+        for (int i = 0; i < n; i++) {
+            numInt[i] = Integer.parseInt(numsStr[i]);
+        }
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(numInt[i])){
+                map.put(numInt[i], map.get(numInt[i]) + 1);
+            }else {
+                map.put(numInt[i],1);
+            }
+
+            int max = -1;
+            for (int j = i+1; j < n; j++) {
+                if (numInt[j] > max){
+                    map.put(numInt[j], map.get(numInt[j]) + 1);
+                    max = numInt[j];
+                }
+            }
+        }
+        int sum = 0;
+        for (Map.Entry<Integer,Integer> map1:map.entrySet()) {
+            sum += (map1.getKey() * map1.getValue());
+        }
+
+    }
+
+    /**
+     * @description: 剩余小鸡总数
+     * @return:
+     * @auther: kami
+     * @date: 2020/3/30 19:15
+     */
+    public static void sumChriken(String nmk,String nums){
+        String[] start = nmk.split(" ");
+        int n = Integer.parseInt(start[0]);
+        int m = Integer.parseInt(start[1]);
+        int k = Integer.parseInt(start[2]);
+        String[] numsStr = nums.split(" ");
+        int[] numInt = new int[n];
+        for (int i = 0; i < n; i++) {
+            numInt[i] = Integer.parseInt(numsStr[i]);
+        }
+
+        for (int i = 0; i < m; i++) {
+            int index = 0;
+            int maxValue = 0;
+            for (int j = 0; j < n; j++) {
+                numInt[j] += k;
+                if (numInt[j] > maxValue){
+                    index = j;
+                    maxValue = numInt[j];
+                }
+            }
+            numInt[index] = maxValue / 2;
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += numInt[i];
+        }
+        System.out.print(sum);
+    }
+
+
+
+
     /**
      * @description: 排列组合
      * 链接：https://www.nowcoder.com/discuss/389778
@@ -38,15 +116,15 @@ public class AliBaBa {
      * @auther: kami
      * @date: 2020/3/26 10:21
      */
-    public static long pow(int n) {
-        if (n == 0)
-            return 1;
-        long half = pow(n / 2);
-        if (n % 2 == 0)
-            return (half * half) % mod;
-        else
-            return (half * half * 2) % mod;
-    }
+//    public static long pow(int n) {
+//        if (n == 0)
+//            return 1;
+//        long half = pow(n / 2);
+//        if (n % 2 == 0)
+//            return (half * half) % mod;
+//        else
+//            return (half * half * 2) % mod;
+//    }
 
 }
 
