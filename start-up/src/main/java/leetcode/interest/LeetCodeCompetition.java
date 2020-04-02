1ccbb34297d079b1a73063e6e7109e8ed861a2c3
kami <zh_guomail@163.com>
2020-04-02 22:58:59 +0800

    30天挑战

---
+++ start-up/src/main/java/leetcode/interest/LeetCodeCompetition.java
@@ -0,0 +1,25 @@
+package leetcode.interest;
+
+/**
+ * @Description: LeetCode30天挑战赛，每天一道题，UTC-8零点开始
+ * @Auther: kami
+ * @Date: 2020/4/2 22:44
+ * @Version: 1.0.0
+ */
+public class LeetCodeCompetition {
+    /**
+     * @description:  Single Number
+     * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+     * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+     * @return:
+     * @auther: kami
+     * @date: 2020/4/2 22:48
+     */
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i:nums) {
+            res ^= i;
+        }
+        return res;
+    }
+}
