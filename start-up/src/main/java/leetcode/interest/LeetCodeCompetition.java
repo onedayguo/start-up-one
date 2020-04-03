52a929e99cf34e575b7fecc8ed57992629ec221c
kami <zh_guomail@163.com>
2020-04-03 08:06:04 +0800

    happy number

--- start-up/src/main/java/leetcode/interest/LeetCodeCompetition.java
+++ start-up/src/main/java/leetcode/interest/LeetCodeCompetition.java
@@ -28,4 +28,18 @@ public class LeetCodeCompetition {
         }
         return nums[0];
     }
+
+    /**
+     * @description: Happy Number
+     * Write an algorithm to determine if a number is "happy".
+     * A happy number is a number defined by the following process: Starting with any positive integer, replace the number
+     * by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or
+     * it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+     * @return:
+     * @auther: kami
+     * @date: 2020/4/3 8:03
+     */
+    public boolean isHappy(int n) {
+
+    }
 }
