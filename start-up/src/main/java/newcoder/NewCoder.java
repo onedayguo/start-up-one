cd46e75a8968d6a3e17041d80d98540f173ceba5
kami <zh_guomail@163.com>
2020-04-11 23:36:23 +0800

    重建二叉树

--- start-up/src/main/java/newcoder/NewCoder.java
+++ start-up/src/main/java/newcoder/NewCoder.java
@@ -79,4 +79,37 @@ public class NewCoder {
         Collections.reverse(list);
         return list;
     }
+
+     // Definition for binary tree
+     public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    /**
+     * @description: 重建二叉树
+     * 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+     * 例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
+     * @return: 二叉树
+     * @auther: kami
+     * @date: 2020/4/11 23:13
+     */
+    public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+        return reConstructBinaryTree(pre,0,pre.length-1,in,0,in.length-1);
+    }
+
+    private TreeNode reConstructBinaryTree(int [] pre,int startPre,int endPre,int [] in,int startIn,int endIn) {
+        if(startPre>endPre||startIn>endIn)
+            return null;
+        TreeNode root=new TreeNode(pre[startPre]);
+        for(int i=startIn;i<=endIn;i++)
+            if(in[i]==pre[startPre]){
+                root.left=reConstructBinaryTree(pre,startPre+1,startPre+i-startIn,in,startIn,i-1);
+                root.right=reConstructBinaryTree(pre,i-startIn+startPre+1,endPre,in,i+1,endIn);
+                break;
+            }
+
+        return root;
+    }
 }
