0cf4d631cdb7f92f72a08ecbfd800bee55338c10
kami <zh_guomail@163.com>
2020-04-19 23:42:38 +0800

    148. Sort List

--- start-up/src/main/java/leetcode/LeetCodeTop100.java
+++ start-up/src/main/java/leetcode/LeetCodeTop100.java
@@ -442,8 +442,50 @@ public class LeetCodeTop100 {
         return count;
     }
 
+    /**
+     * @description: 148. Sort List
+     * Sort a linked list in O(n log n) time using constant space complexity.
+     * Example 1:
+     * Input: 4->2->1->3
+     * Output: 1->2->3->4
+     * Example 2:
+     * Input: -1->5->3->4->0
+     * Output: -1->0->3->4->5
+     * @return:
+     * @auther: kami
+     * @date: 2020/4/19 20:20
+     */
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode slow = head, fast = head, pre = head;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = null;
+        return merge(sortList(head), sortList(slow));
+    }
+    public ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = merge(l1, l2.next);
+            return l2;
+        }
+    }
+
     public static void main(String[] args) {
-        boolean[] am = new boolean[10];
-        System.out.println(Arrays.toString(am));
+        ListNode head = new ListNode(4);
+        ListNode head1 = new ListNode(2);
+        ListNode head2 = new ListNode(1);
+        ListNode head3 = new ListNode(3);
+        head.next = head1;
+        head1.next = head2;
+        head2.next = head3;
+        System.out.println();
     }
 }
