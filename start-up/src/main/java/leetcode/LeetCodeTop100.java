7f42c7eb2fc01c34ecd1df7c4c7cf463ccc663bf
kami <zh_guomail@163.com>
2020-04-19 16:15:16 +0800

    416. Partition Equal Subset Sum

--- start-up/src/main/java/leetcode/LeetCodeTop100.java
+++ start-up/src/main/java/leetcode/LeetCodeTop100.java
@@ -201,6 +201,28 @@ public class LeetCodeTop100 {
         return sum < S || (S + sum) % 2 > 0 ? 0 : subsetSum(nums, (S + sum) >>> 1);
 
     }
+    //https://blog.csdn.net/hit0803107/article/details/54894227
+    public int findTargetSumWays1(int[] nums, int S) {
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        int sum = 0;
+        for (int num : nums){
+            sum += num;
+        }
+        if (sum < S || (sum + S) % 2 != 0){
+            return 0;
+        }
+        sum = (sum + S) / 2;
+        int[] dp = new int[sum + 1];
+        dp[0] = 1;
+        for (int num : nums){
+            for (int i = sum; i >= num; i--){
+                dp[i] += dp[i - num];
+            }
+        }
+        return dp[sum];
+    }
     /**
      * @description: 494. Target Sum,暴力求解法
      * @return:
@@ -247,13 +269,93 @@ public class LeetCodeTop100 {
         return dp[s];
     }
 
+    /**
+     * @description: 416. Partition Equal Subset Sum
+     * Given a non-empty array containing only positive integers, find if the array can be partitioned into two
+     * subsets such that the sum of elements in both subsets is equal.
+     * Note:
+     * Each of the array element will not exceed 100.
+     * The array size will not exceed 200.
+     *
+     * This problem is essentially let us to find whether there are several numbers in a set which are able to sum to a
+     * specific value (in this problem, the value is sum/2).
+     * Actually, this is a 0/1 knapsack problem, for each number, we can pick it or not. Let us assume dp[i][j] means
+     * whether the specific sum j can be gotten from the first i numbers. If we can pick such a series of numbers from
+     * 0-i whose sum is j, dp[i][j] is true, otherwise it is false.
+     * Base case: dp[0][0] is true; (zero number consists of sum 0 is true)
+     * Transition function: For each number, if we don't pick it, dp[i][j] = dp[i-1][j], which means if the first i-1
+     * elements has made it to j, dp[i][j] would also make it to j (we can just ignore nums[i]). If we pick nums[i].
+     * dp[i][j] = dp[i-1][j-nums[i]], which represents that j is composed of the current value nums[i] and the remaining
+     * composed of other previous numbers. Thus, the transition function is dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i]]
+     * @return:
+     * @auther: kami
+     * @date: 2020/4/19 14:02
+     */
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+        }
+
+        if ((sum & 1) == 1) {
+            return false;
+        }
+        sum /= 2;
+
+        int n = nums.length;
+        boolean[][] dp = new boolean[n+1][sum+1];
+
+        for (int i = 0; i < n+1; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < sum+1; j++) {
+                dp[i][j] = dp[i-1][j];
+                if (j >= nums[i-1]) {
+                    dp[i][j] = (dp[i][j] || dp[i-1][j-nums[i-1]]);
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+    /**
+     * @description: But can we optimize it? It seems that we cannot optimize it in time. But we can optimize in space.
+     * We currently use two dimensional array to solve it, but we can only use one dimensional array.
+     * @return:
+     * @auther: kami
+     * @date: 2020/4/19 16:10
+     */
+    public boolean canPartition1(int[] nums) {
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+        }
+
+        if ((sum & 1) == 1) {
+            return false;
+        }
+        sum /= 2;
+
+        boolean[] dp = new boolean[sum+1];
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int i = sum; i > 0; i--) {
+                if (i >= num) {
+                    dp[i] = dp[i] || dp[i-num];
+                }
+            }
+        }
+
+        return dp[sum];
+    }
+
 
     public static void main(String[] args) {
-        String s = "cars";
-        List<String> list = new ArrayList<>();
-        list.add("car");
-        list.add("ca");
-        list.add("rs");
-        System.out.println(wordBreak(s,list));
+        boolean[] am = new boolean[10];
+        System.out.println(Arrays.toString(am));
     }
 }
