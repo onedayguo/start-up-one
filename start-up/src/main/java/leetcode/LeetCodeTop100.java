cddcfd41ee8bb0b80b0b6e500d9f9bde296a971a
kami <zh_guomail@163.com>
2020-04-22 08:15:46 +0800

    239. Sliding Window Maximum

--- start-up/src/main/java/leetcode/LeetCodeTop100.java
+++ start-up/src/main/java/leetcode/LeetCodeTop100.java
@@ -478,6 +478,40 @@ public class LeetCodeTop100 {
         }
     }
 
+    /**
+     * @description: 239. Sliding Window Maximum
+     * Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the
+     * very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+     * Return the max sliding window.
+        Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+        Output: [3,3,5,5,6,7]
+        Explanation:
+
+        Window position                Max
+        ---------------               -----
+        [1  3  -1] -3  5  3  6  7       3
+        1 [3  -1  -3] 5  3  6  7       3
+        1  3 [-1  -3  5] 3  6  7       5
+        1  3  -1 [-3  5  3] 6  7       5
+        1  3  -1  -3 [5  3  6] 7       6
+        1  3  -1  -3  5 [3  6  7]      7
+     * Follow up:
+     * Could you solve it in linear time?
+     * @return: 滑动窗口最大值组成的列表
+     * @auther: kami
+     * @date: 2020/4/22 6:50
+     */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int resLen = nums.length-k+1;
+        int[] resArr = new int[resLen];
+        List<Integer> slidList = new LinkedList<>();
+
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (slidList.isEmpty() || slidList.get)
+        }
+    }
+
     public static void main(String[] args) {
         ListNode head = new ListNode(4);
         ListNode head1 = new ListNode(2);
