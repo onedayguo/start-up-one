eedd64c1a6aebc616c64a877b24f1cc75bf80967
kami <zh_guomail@163.com>
2020-04-23 22:11:42 +0800

    Product of Array Except Self

--- start-up/src/main/java/leetcode/interest/LeetCodeCompetition.java
+++ start-up/src/main/java/leetcode/interest/LeetCodeCompetition.java
@@ -429,6 +429,35 @@ public class LeetCodeCompetition {
 
     }
 
+    /**
+     * @description: Product of Array Except Self
+     * Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the
+     * product of all the elements of nums except nums[i].
+     * onstraint: It's guaranteed that the product of the elements of any prefix or suffix of the array (including the
+     * whole array) fits in a 32 bit integer.
+     *
+     * Note: Please solve it without division and in O(n).
+     *
+     * Follow up:
+     * Could you solve it with constant space complexity? (The output array does not count as extra space for the
+     * purpose of space complexity analysis.)
+     * @return: 其他位置相乘之积组成的数组
+     * @auther: kami
+     * @date: 2020/4/23 21:33
+     */
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length, right = 1;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
     public static void main(String[] args) {
         LeetCodeCompetition main = new LeetCodeCompetition();
         int[][] shift = {{0,7},{1,7},{1,0},{1,3},{0,3},{0,6},{1,2}};
