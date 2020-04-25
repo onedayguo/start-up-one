007acaaaeb4bf9c5c0e73aee342a9561e94be5e0
kami <zh_guomail@163.com>
2020-04-26 07:50:42 +0800

    Number of Islands

--- start-up/src/main/java/leetcode/interest/LeetCodeCompetition.java
+++ start-up/src/main/java/leetcode/interest/LeetCodeCompetition.java
@@ -547,6 +547,59 @@ public class LeetCodeCompetition {
         }
         return  low == 0;
     }
+
+    /**
+     * @description: Number of Islands
+     * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by
+     * water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges
+     * of the grid are all surrounded by water.
+     * @return: 小岛个数
+     * @auther: kami
+     * @date: 2020/4/25 9:17
+     */
+    public int numIslands(char[][] grid) {
+        // 参数校验
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        // 元素默认值是false
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                // 如果此位置没有被访问过，并且此位置是岛，广度优先遍历
+                if (!visited[i][j] && grid[i][j] == '1') {
+                    result++;
+                    bfs(grid, visited, i, j);
+                }
+            }
+        }
+        return result;
+    }
+
+    private void bfs(char[][] grid, boolean[][] visited, int row, int col) {
+
+        if (row >= 0 && row < grid.length // 行合法
+                && col >= 0 && col < grid[0].length // 列合法
+                && !visited[row][col] // 没有访问过
+                && grid[row][col] == '1') { // 是岛上陆地
+
+            // 标记此位置已经访问过了
+            visited[row][col] = true;
+
+            // 上
+            bfs(grid, visited, row - 1, col);
+            // 右
+            bfs(grid, visited, row, col + 1);
+            // 下
+            bfs(grid, visited, row + 1, col);
+            // 左
+            bfs(grid, visited, row, col - 1);
+
+        }
+    }
     public static void main(String[] args) {
         LeetCodeCompetition main = new LeetCodeCompetition();
         int[][] shift = {{0,7},{1,7},{1,0},{1,3},{0,3},{0,6},{1,2}};
