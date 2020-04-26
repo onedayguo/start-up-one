56e3474540ea8f5e4f7c556a1c01843ad6718a91
kami <zh_guomail@163.com>
2020-04-26 22:45:11 +0800

    Minimum Path Sum

--- start-up/src/main/java/leetcode/interest/LeetCodeCompetition.java
+++ start-up/src/main/java/leetcode/interest/LeetCodeCompetition.java
@@ -612,7 +612,13 @@ public class LeetCodeCompetition {
      * @date: 2020/4/26 7:52
      */
     public int minPathSum(int[][] grid) {
-
+        int[] dp = new int[grid.length];
+        dp[0] = grid[0][0];
+        for(int i=1; i<grid.length; i++) dp[i] = grid[i][0]+dp[i-1];
+        for(int j=1; j<grid[0].length; j++)
+            for(int i=0; i<grid.length; i++)
+                dp[i] = (i==0 ? dp[i] : Math.min(dp[i], dp[i-1])) + grid[i][j];
+        return dp[grid.length-1];
     }
     public static void main(String[] args) {
         LeetCodeCompetition main = new LeetCodeCompetition();
