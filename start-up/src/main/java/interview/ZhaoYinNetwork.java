7af91bce6c358b80cbf55a73326b365ee30b4538
kami <zh_guomail@163.com>
2020-05-05 16:34:24 +0800

    招银网络

---
+++ start-up/src/main/java/interview/ZhaoYinNetwork.java
@@ -0,0 +1,20 @@
+package interview;
+
+import java.util.Scanner;
+
+/**
+ * @Description: 招银网络
+ * @Auther: kami
+ * @Date: 2020/5/5 13:57
+ * @Version: 1.0.0
+ */
+public class ZhaoYinNetwork {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            int n = input.nextInt();
+
+        }
+    }
+}
