c9fe948fe05ce63d6990e7110a1112c88cb44d73
kami <zh_guomail@163.com>
2020-05-06 22:17:42 +0800

    297. Serialize and Deserialize Binary Tree

--- start-up/src/main/java/leetcode/LeetCodeTop100.java
+++ start-up/src/main/java/leetcode/LeetCodeTop100.java
@@ -588,6 +588,31 @@ public class LeetCodeTop100 {
         return diff == 0?0:diff+1;
     }
 
+    /**
+     * @description: 297. Serialize and Deserialize Binary Tree
+     * Serialization is the process of converting a data structure or object into a sequence of bits so that it can be
+     * stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in
+     * the same or another computer environment.
+     * Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your
+     * serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be
+     * serialized to a string and this string can be deserialized to the original tree structure.
+     * @return:
+     * @auther: kami
+     * @date: 2020/5/6 21:57
+     */
+    public class Codec {
+
+        // Encodes a tree to a single string.
+        public String serialize(TreeNode root) {
+
+        }
+
+        // Decodes your encoded data to tree.
+        public TreeNode deserialize(String data) {
+
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums = {2,6,4,8,10,9,15};
         System.out.println(findUnsortedSubarray(nums));
