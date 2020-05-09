565483828aa985a6f7d539126fdf1e50c343c388
kami <zh_guomail@163.com>
2020-05-09 21:41:42 +0800

    297. Serialize and Deserialize Binary Tree

--- start-up/src/main/java/leetcode/LeetCodeTop100.java
+++ start-up/src/main/java/leetcode/LeetCodeTop100.java
@@ -601,15 +601,40 @@ public class LeetCodeTop100 {
      * @date: 2020/5/6 21:57
      */
     public class Codec {
+        private static final String spliter = ",";
+        private static final String NN = "X";
 
         // Encodes a tree to a single string.
         public String serialize(TreeNode root) {
-
+            StringBuilder sb = new StringBuilder();
+            buildString(root, sb);
+            return sb.toString();
         }
 
+        private void buildString(TreeNode node, StringBuilder sb) {
+            if (node == null) {
+                sb.append(NN).append(spliter);
+            } else {
+                sb.append(node.val).append(spliter);
+                buildString(node.left, sb);
+                buildString(node.right,sb);
+            }
+        }
         // Decodes your encoded data to tree.
         public TreeNode deserialize(String data) {
+            Deque<String> nodes = new LinkedList<>(Arrays.asList(data.split(spliter)));
+            return buildTree(nodes);
+        }
 
+        private TreeNode buildTree(Deque<String> nodes) {
+            String val = nodes.remove();
+            if (val.equals(NN)) return null;
+            else {
+                TreeNode node = new TreeNode(Integer.parseInt(val));
+                node.left = buildTree(nodes);
+                node.right = buildTree(nodes);
+                return node;
+            }
         }
     }
 
