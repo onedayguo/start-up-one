876737638867d4501ebfd1970d78d225c2608e1d
kami <zh_guomail@163.com>
2020-05-15 22:22:29 +0800

    91. Decode Ways

--- start-up/src/main/java/leetcode/LeetCode6.java
+++ start-up/src/main/java/leetcode/LeetCode6.java
@@ -20,18 +20,20 @@ public class LeetCode6 {
      * @auther: kami
      * @date: 2020/5/13 16:56
      */
-
     public static int numDecodings(String s) {
         int n = s.length();
         if (n == 0) return 0;
 
-        int[] memo = new int[n+1];
-        memo[n]  = 1;
-        memo[n-1] = s.charAt(n-1) != '0' ? 1 : 0;
+        int[] memo = new int[n + 1];//初始化元素默认0
+        memo[n] = 1; //额外的计数设置为1，保证字符串至少有一种解码方式
+        memo[n - 1] = s.charAt(n - 1) == '0' ? 0 : 1; //
 
-        for (int i = n - 2; i >= 0; i--)
-            if (s.charAt(i) == '0') continue;
-            else memo[i] = (Integer.parseInt(s.substring(i,i+2))<=26) ? memo[i+1]+memo[i+2] : memo[i+1];
+        for (int i = n - 2; i >= 0; i--){
+            if (s.charAt(i) !='0'){
+                //动态规划思想；如果从当前位置向后截取两位，值小于等于26的话，则需累积上一步的基数，否则与上一步保持一致
+                memo[i] = (Integer.parseInt(s.substring(i, i + 2)) <= 26) ? memo[i + 1] + memo[i + 2] : memo[i + 1];
+            }
+        }
 
         return memo[0];
     }
