9591aeb8c14499955d9b3d64677dba5bf353ddb4
kami <zh_guomail@163.com>
2020-05-31 11:35:03 +0800

    牛客后端技术020

---
+++ start-up/src/main/java/interview/NewCoder.java
@@ -0,0 +1,23 @@
+package interview;
+
+import java.util.Scanner;
+
+/**
+ * @Description: 笔试信息
+ * 试卷名称：牛客后端技术020
+ * 考试时间：长期有效(考试时长60分钟)
+ * 考试地址：https://exam.nowcoder.com/cts/17072434/summary?id=8EB17BD913583B18
+ * （考试地址为你的私人专属地址，请勿转发。如无法直接打开，请拷贝完整链接并粘贴至浏览器地址栏）
+ * @Author: kami
+ * @Date: 2020/5/31 11:33
+ * @Version: 1.0.0
+ */
+public class NewCoder {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String nHitT = in.nextLine();
+            String hpStr = in.nextLine();
+        }
+    }
+}
