fa94201de94879d794de17b418ad76f000b716bf
kami <zh_guomail@163.com>
2020-06-07 08:47:19 +0800

    406. Queue Reconstruction by Height

--- start-up/src/main/java/leetcode/LeetCodeTop100.java
+++ start-up/src/main/java/leetcode/LeetCodeTop100.java
@@ -1025,8 +1025,45 @@ public class LeetCodeTop100 {
         Arrays.sort(nums);
         return nums[nums.length - k];
     }
-    public static void main(String[] args) {
 
+    /**
+     * @description: 406. Queue Reconstruction by Height
+     * Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers
+     * (h, k), where h is the height of the person and k is the number of people in front of this person who have a
+     * height greater than or equal to h. Write an algorithm to reconstruct the queue.
+     * Note:
+     * The number of people is less than 1,100.
+     * @return:
+     * @author: kami
+     * @date: 2020/6/6 20:16
+     */
+    public static int[][] reconstructQueue(int[][] people) {
+        /**
+         * @description: Pick out tallest group of people and sort them in a subarray (S). Since there's no other groups
+         * of people taller than them, therefore each guy's index will be just as same as his k value.
+         * For 2nd tallest group (and the rest), insert each one of them into (S) by k value. So on and so forth.
+         * E.g.
+         * input: [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+         * subarray after step 1: [[7,0], [7,1]]
+         * subarray after step 2: [[7,0], [6,1], [7,1]]
+         * ...
+         * @return: int[][]
+         * @author: kami
+         * @date: 2020/6/7 8:45
+         */
+        // pick up the tallest guy first
+        // when insert the next tall guy, just need to insert him into kth position
+        // repeat until all people are inserted into list
+        Arrays.sort(people, (o1, o2) -> o1[0] != o2[0] ?  o2[0] - o1[0] : o1[1] - o2[1]);
+        List<int[]> res = new LinkedList<>();
+        for(int[] cur : people){
+            res.add(cur[1],cur);
+        }
+        return res.toArray(new int[people.length][]);
+    }
+    public static void main(String[] args) {
+        int[][] arr = {{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
+        reconstructQueue(arr);
 
     }
 
