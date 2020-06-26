a0473ee7d46cbf46f6e0c828987636db978d7642
kami <zh_guomail@163.com>
2020-06-26 11:26:50 +0800

    337 house robber 3

--- start-up/src/main/java/leetcode/LeetCodeTop100.java
+++ start-up/src/main/java/leetcode/LeetCodeTop100.java
@@ -1368,6 +1368,21 @@ public class LeetCodeTop100 {
         return right;
     }
 
+    /**
+     * @description: 337. House Robber III
+     * The thief has found himself a new place for his thievery again. There is only one entrance to this area,
+     * called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart
+     * thief realized that "all houses in this place forms a binary tree". It will automatically contact the police
+     * if two directly-linked houses were broken into on the same night.
+     * Determine the maximum amount of money the thief can rob tonight without alerting the police.
+     * @return:
+     * @author: kami
+     * @date: 2020/6/26 11:24
+     */
+    public int rob(TreeNode root) {
+        return 0;
+    }
+
     public static void main(String[] args) throws InterruptedException {
         TreeNode root = new TreeNode(3);
         root.left = new TreeNode(1);
