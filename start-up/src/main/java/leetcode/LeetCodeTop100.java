3a464673d900e0de1f99120645482deb567ab34f
kami <zh_guomail@163.com>
2020-06-27 11:03:21 +0800

    337. House Robber III

--- start-up/src/main/java/leetcode/LeetCodeTop100.java
+++ start-up/src/main/java/leetcode/LeetCodeTop100.java
@@ -1380,11 +1380,17 @@ public class LeetCodeTop100 {
      * @date: 2020/6/26 11:24
      */
     public int rob(TreeNode root) {
-        return 0;
-        //this is a test
-        //another test
-        //123456
-        ///////6165dfgsfgsfgsfg
+        int[] num = dfs(root);
+        return Math.max(num[0], num[1]);
+    }
+    private int[] dfs(TreeNode x) {
+        if (x == null) return new int[2];
+        int[] left = dfs(x.left);
+        int[] right = dfs(x.right);
+        int[] res = new int[2];
+        res[0] = left[1] + right[1] + x.val;
+        res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return res;
     }
 
     public static void main(String[] args) throws InterruptedException {
