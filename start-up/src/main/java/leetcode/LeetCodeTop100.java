8fb8c865c19ceb1e10702de2324539aad7d1fdf7
kami <zh_guomail@163.com>
2020-07-25 22:24:45 +0800

    138. Copy List with Random Pointer

--- start-up/src/main/java/leetcode/LeetCodeTop100.java
+++ start-up/src/main/java/leetcode/LeetCodeTop100.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import org.w3c.dom.Node;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -1501,6 +1503,34 @@ public class LeetCodeTop100 {
         return res;
     }
 
+    /**
+     * @description: 138. Copy List with Random Pointer
+     * A linked list is given such that each node contains an additional random pointer which could point to any node
+     * in the list or null.
+     * Return a deep copy of the list.
+     * The Linked List is represented in the input/output as a list of n nodes. Each node is represented as a pair of
+     * [val, random_index] where:
+     * val: an integer representing Node.val
+     * random_index: the index of the node (range from 0 to n-1) where random pointer points to, or null if it does not
+     * point to any node.
+     * @return:
+     * @author: kami
+     * @date: 2020/7/25 22:23
+     */
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+    public Node copyRandomList(Node head) {
+        
+    }
     public static void main(String[] args) throws InterruptedException {
         TreeNode root = new TreeNode(3);
         root.left = new TreeNode(1);
