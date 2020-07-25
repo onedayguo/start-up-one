d08781e5c4a9eed8cdcb74b3c95b88a7e7e2db6d
kami <zh_guomail@163.com>
2020-07-25 22:17:55 +0800

    128. Longest Consecutive Sequence

--- start-up/src/main/java/leetcode/LeetCodeTop100.java
+++ start-up/src/main/java/leetcode/LeetCodeTop100.java
@@ -1456,6 +1456,51 @@ public class LeetCodeTop100 {
         return Math.max(left, right) + node.val;
     }
 
+    /**
+     * @description: 128. Longest Consecutive Sequence
+     * Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+     * Your algorithm should run in O(n) complexity.
+     * Example:
+     * Input: [100, 4, 200, 1, 3, 2]
+     * Output: 4
+     * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+     * We will use HashMap. The key thing is to keep track of the sequence length and store that in the boundary points
+     * of the sequence. For example, as a result, for sequence {1, 2, 3, 4, 5}, map.get(1) and map.get(5) should both
+     * return 5.
+     * Whenever a new element n is inserted into the map, do two things:
+     * See if n - 1 and n + 1 exist in the map, and if so, it means there is an existing sequence next to n. Variables
+     * left and right will be the length of those two sequences, while 0 means there is no sequence and n will be the
+     * boundary point later. Store (left + right + 1) as the associated value to key n into the map.
+     * Use left and right to locate the other end of the sequences to the left and right of n respectively, and replace
+     * the value with the new length.
+     * @return: 最长连续数字的长度
+     * @author: kami
+     * @date: 2020/7/25 21:52
+     */
+    public int longestConsecutive(int[] nums) {
+        int res = 0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int n : nums) {
+            if (!map.containsKey(n)) {
+                int left = map.getOrDefault(n - 1, 0);
+                int right = map.getOrDefault(n + 1, 0);
+                // sum: length of the sequence n is in
+                int sum = left + right + 1;
+                map.put(n, sum);
+
+                // keep track of the max length
+                res = Math.max(res, sum);
+
+                // extend the length to the boundary(s)
+                // of the sequence
+                // will do nothing if n has no neighbors
+                map.put(n - left, sum);
+                map.put(n + right, sum);
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) throws InterruptedException {
         TreeNode root = new TreeNode(3);
         root.left = new TreeNode(1);
