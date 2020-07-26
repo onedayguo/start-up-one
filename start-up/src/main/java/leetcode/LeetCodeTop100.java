9316a79de78836e8d10b98b8f059f0f6ce21968b
kami <zh_guomail@163.com>
2020-07-26 22:36:19 +0800

    138. Copy List with Random Pointer

--- start-up/src/main/java/leetcode/LeetCodeTop100.java
+++ start-up/src/main/java/leetcode/LeetCodeTop100.java
@@ -1517,7 +1517,7 @@ public class LeetCodeTop100 {
      * @author: kami
      * @date: 2020/7/25 22:23
      */
-    class Node {
+    static class Node {
         int val;
         Node next;
         Node random;
@@ -1529,7 +1529,49 @@ public class LeetCodeTop100 {
         }
     }
     public Node copyRandomList(Node head) {
-        
+        Node iter = head, next;
+
+        // First round: make copy of each node,
+        // and link them together side-by-side in a single list.
+        while (iter != null) {
+            next = iter.next;
+
+            Node copy = new Node(iter.val);
+            iter.next = copy;
+            copy.next = next;
+
+            iter = next;
+        }
+
+        // Second round: assign random pointers for the copy nodes.
+        iter = head;
+        while (iter != null) {
+            if (iter.random != null) {
+                iter.next.random = iter.random.next;
+            }
+            iter = iter.next.next;
+        }
+
+        // Third round: restore the original list, and extract the copy list.
+        iter = head;
+        Node pseudoHead = new Node(0);
+        Node copy, copyIter = pseudoHead;
+
+        while (iter != null) {
+            next = iter.next.next;
+
+            // extract the copy
+            copy = iter.next;
+            copyIter.next = copy;
+            copyIter = copy;
+
+            // restore the original list
+            iter.next = next;
+
+            iter = next;
+        }
+
+        return pseudoHead.next;
     }
     public static void main(String[] args) throws InterruptedException {
         TreeNode root = new TreeNode(3);
