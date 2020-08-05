fd57a856113b2f6d32c34f4361c2baf7f1939b5b
kami <zh_guomail@163.com>
2020-08-05 22:16:27 +0800

    152. Maximum Product Subarray

--- start-up/src/main/java/leetcode/tree/LeetcodeFdd.java
+++ start-up/src/main/java/leetcode/tree/LeetcodeFdd.java
@@ -56,6 +56,18 @@ public class LeetcodeFdd {
      * @date: 2020/8/4 22:30
      */
     public int maxProduct(int[] nums) {
-        return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int max = nums[0], min = nums[0], result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int temp = max;
+            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+            min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
+            if (max > result) {
+                result = max;
+            }
+        }
+        return result;
     }
 }
