8753c04d4dda04f23d5f22123d0e0b97c958a6e8
kami <kami>
2020-09-21 16:33:19 +0800

    212. Word Search II

--- start-up/src/main/java/leetcode/LeetcodeFdd.java
+++ start-up/src/main/java/leetcode/LeetcodeFdd.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -148,7 +149,68 @@ public class LeetcodeFdd {
      * @Author: kami
      * @Date: 2020/9/8 9:51
      */
-    public List<String> findWords(char[][] board, String[] words) {
-        return null;
+    public static List<String> findWords(char[][] board, String[] words) {
+        List<String> res = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs (board, i, j, root, res);
+            }
+        }
+        return res;
+    }
+
+    public static void dfs(char[][] board, int i, int j, TrieNode p, List<String> res) {
+        char c = board[i][j];
+        if (c == '#' || p.next[c - 'a'] == null) {
+            return;
+        }
+        p = p.next[c - 'a'];
+        if (p.word != null) {   // found one
+            res.add(p.word);
+            p.word = null;     // de-duplicate
+        }
+
+        board[i][j] = '#';
+        if (i > 0) {
+            dfs(board, i - 1, j ,p, res);
+        }
+        if (j > 0) {
+            dfs(board, i, j - 1, p, res);
+        }
+        if (i < board.length - 1) {
+            dfs(board, i + 1, j, p, res);
+        }
+        if (j < board[0].length - 1) {
+            dfs(board, i, j + 1, p, res);
+        }
+        board[i][j] = c;
+    }
+
+    public static TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String w : words) {
+            TrieNode p = root;
+            for (char c : w.toCharArray()) {
+                int i = c - 'a';
+                if (p.next[i] == null) {
+                    p.next[i] = new TrieNode();
+                }
+                p = p.next[i];
+            }
+            p.word = w;
+        }
+        return root;
+    }
+
+    static class TrieNode {
+        TrieNode[] next = new TrieNode[26];
+        String word;
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"happy","hp","hello","nothing"};
+
+        findWords(new char[2][2],words);
     }
 }
