9304552a985eba2c91857316ccc002f9ac481171
kami <zh_guomail@163.com>
2021-01-26 22:48:01 +0800

    【多线程】测试并发

---
+++ start-up/src/main/java/test/thread/ThreadUnsafeExample.java
@@ -0,0 +1,42 @@
+package test.thread;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @Description: 计数
+ * @Author: kami
+ * @Date: 2021/1/26 22:37
+ * @Version: 1.0.0
+ */
+public class ThreadUnsafeExample {
+
+    private int cnt = 0;
+
+    public void add() {
+        cnt++;
+    }
+
+    public int get() {
+        return cnt;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        int threadSize = 1000;
+        ThreadUnsafeExample example = new ThreadUnsafeExample();
+        final CountDownLatch countDownLatch = new CountDownLatch(threadSize);
+        ExecutorService executorService = Executors.newCachedThreadPool();;
+        for (int i = 0; i < threadSize; i++) {
+            executorService.execute(() -> {
+                example.add();
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        System.out.println(example.get());
+    }
+}
