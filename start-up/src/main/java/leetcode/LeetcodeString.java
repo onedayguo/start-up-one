b15a193a2468cbac743901388e76d4e9a33a1944
kami <kami>
2021-03-02 21:50:17 +0800

    [solve]1678. Goal Parser Interpretation

--- start-up/src/main/java/leetcode/LeetcodeString.java
+++ start-up/src/main/java/leetcode/LeetcodeString.java
@@ -72,4 +72,20 @@ public class LeetcodeString {
         return (int)items.stream().filter(e -> ruleValue.equals(e.get(finalTh))).count();
 
     }
+    /**
+     * @Description: 1678. Goal Parser Interpretation
+     * You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G",
+     * "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o",
+     * and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+     *
+     * Given the string command, return the Goal Parser's interpretation of command.
+     * @Param:
+     * @Return: 
+     * @Author: kami
+     * @Date: 2021/3/2 21:46
+     */
+    public String interpret(String command) {
+        return command.replace("(al)", "al").replace("()", "o");
+
+    }
 }
