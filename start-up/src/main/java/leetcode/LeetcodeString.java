639852c235edba43ea8fe15da3522d896592c4eb
kami <kami>
2021-03-02 21:45:51 +0800

    [solve]1773. Count Items Matching a Rule

--- start-up/src/main/java/leetcode/LeetcodeString.java
+++ start-up/src/main/java/leetcode/LeetcodeString.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.List;
+
 /**
  * @Description:
  * @Author: kami
@@ -40,5 +42,34 @@ public class LeetcodeString {
     }
     private  boolean isNumOrChar(char a){
         return (a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z') || (a >= '0' && a <= '9');
+    }
+
+    /**
+     * @Description: 1773. Count Items Matching a Rule
+     * You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name
+     * of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+     *
+     * The ith item is said to match the rule if one of the following is true:
+     *
+     * ruleKey == "type" and ruleValue == typei.
+     * ruleKey == "color" and ruleValue == colori.
+     * ruleKey == "name" and ruleValue == namei.
+     * Return the number of items that match the given rule.
+     * @Param:
+     * @Return: 
+     * @Author: kami
+     * @Date: 2021/3/2 21:37
+     */
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int th = 0;
+        if ("color".equals(ruleKey)){
+            th = 1;
+        }else if ("name".equals(ruleKey)){
+            th = 2;
+        }
+        int finalTh = th;
+        int count = 0;
+        return (int)items.stream().filter(e -> ruleValue.equals(e.get(finalTh))).count();
+
     }
 }
