86d97002c4e312a4ad071291bdccfa3b25755de5
kami <zh_guomail@163.com>
2021-03-07 23:28:16 +0800

    [算法]983

--- start-up/src/main/java/leetcode/dynamic/Dynamic.java
+++ start-up/src/main/java/leetcode/dynamic/Dynamic.java
@@ -589,6 +589,29 @@ public class Dynamic {
         }
         return res;
     }
+    
+    /**
+     * @description: 983. Minimum Cost For Tickets
+     * In a country popular for train travel, you have planned some train travelling one year in advance.
+     * The days of the year that you will travel is given as an array days.  Each day is an integer from 1 to 365.
+     *
+     * Train tickets are sold in 3 different ways:
+     *
+     * a 1-day pass is sold for costs[0] dollars;
+     * a 7-day pass is sold for costs[1] dollars;
+     * a 30-day pass is sold for costs[2] dollars.
+     * The passes allow that many days of consecutive travel.  For example, if we get a 7-day pass on day 2,
+     * then we can travel for 7 days: day 2, 3, 4, 5, 6, 7, and 8.
+     *
+     * Return the minimum number of dollars you need to travel every day in the given list of days.
+     * @return: TODO
+     * @author: kami
+     * @备注：TODO
+     * @date: 2021/3/7 23:23
+     */
+    public int mincostTickets(int[] days, int[] costs) {
+        return 0;
+    }
 
     private static String str = "test";
     public static void main(String[] args) {
