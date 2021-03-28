1fd551bf693e6d348bde5b4c51e1a489284c6a54
kami <zh_guomail@163.com>
2021-03-28 22:34:47 +0800

    [算法] 1219. Path with Maximum Gold

--- start-up/src/main/java/leetcode/backtracking/BackTracking.java
+++ start-up/src/main/java/leetcode/backtracking/BackTracking.java
@@ -389,6 +389,48 @@ public class BackTracking {
         return sb.toString();
     }
 
+    /**
+     * @description: 1219. Path with Maximum Gold
+     * In a gold mine grid of size m * n, each cell in this mine has an integer representing the amount of
+     * gold in that cell, 0 if it is empty.
+     *
+     * Return the maximum amount of gold you can collect under the conditions:
+     *
+     * Every time you are located in a cell you will collect all the gold in that cell.
+     * From your position you can walk one step to the left, right, up or down.
+     * You can't visit the same cell more than once.
+     * Never visit a cell with 0 gold.
+     * You can start and stop collecting gold from any position in the grid that has some gold.
+     * @return: 获取到最大的金子数量
+     * @author: kami
+     * @备注： 回朔
+     * @date: 2021/3/28 19:28
+     */
+    public int getMaximumGold(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int maxGold = 0;
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                maxGold = Math.max(maxGold, findMaxGold(grid, m, n, r, c));
+            }
+        }
+        return maxGold;
+    }
+    int[] DIR = new int[]{0, 1, 0, -1, 0};
+    int findMaxGold(int[][] grid, int m, int n, int r, int c) {
+        if (r < 0 || r == m || c < 0 || c == n || grid[r][c] == 0) {
+            return 0;
+        }
+        int origin = grid[r][c];
+        grid[r][c] = 0; // mark as visited
+        int maxGold = 0;
+        for (int i = 0; i < 4; i++) {
+            maxGold = Math.max(maxGold, findMaxGold(grid, m, n, DIR[i] + r, DIR[i+1] + c));
+        }
+        grid[r][c] = origin; // backtrack
+        return maxGold + origin;
+    }
+
     public static void main(String[] args) {
         System.out.println(numberOfMatches(7));
     }
---
+++ start-up/src/main/java/lock/AbstractQueuedSynchonizerDemo.java
@@ -0,0 +1,50 @@
+package lock;
+
+
+
+
+import java.util.HashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class MyThread extends Thread {
+    private Lock lock;
+    public MyThread(String name, Lock lock) {
+        super(name);
+        this.lock = lock;
+    }
+
+    @Override
+    public void run () {
+        lock.lock();
+        try {
+            System.out.println(Thread.currentThread() + " running");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                HashMap
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+/**
+ * @Description: 锁测试
+ * @Author: kami
+ * @Date: 2021/3/17 21:52
+ * @Version: 1.0.0
+ */
+public class AbstractQueuedSynchonizerDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Lock lock = new ReentrantLock(true);
+
+        MyThread t1 = new MyThread("t1", lock);
+        MyThread t2 = new MyThread("t2", lock);
+        MyThread t3 = new MyThread("t3", lock);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
