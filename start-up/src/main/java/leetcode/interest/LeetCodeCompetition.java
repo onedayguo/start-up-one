87a9586c6c3b7fcbe9e7940f3914e0bf9e400f54
kami <zh_guomail@163.com>
2021-04-25 21:24:03 +0800

    [周挑战]Rotate Image翻转90度：上下翻转-》对称翻转实现顺时针翻转90度

--- start-up/src/main/java/leetcode/interest/LeetCodeCompetition.java
+++ start-up/src/main/java/leetcode/interest/LeetCodeCompetition.java
@@ -1382,6 +1382,43 @@ public class LeetCodeCompetition {
         }
         return map;
     }
+    /**
+     * @description: Rotate Image
+     * You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+     *
+     * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+     * DO NOT allocate another 2D matrix and do the rotation.
+     * /*
+     *  * clockwise rotate
+     *  * first reverse up to down, then swap the symmetry
+     *  * 1 2 3     7 8 9     7 4 1
+     *  * 4 5 6  => 4 5 6  => 8 5 2
+     *  * 7 8 9     1 2 3     9 6 3
+     * @return: 直接修改原数组，顺时针旋转90度
+     * @author: kami
+     * @关键词： 上下翻转，对称翻转实现顺时针；左右翻转，对称翻转实现逆时针
+     * @date: 2021/4/25 20:28
+     */
+    public void rotate(int[][] matrix) {
+        // 上下翻转
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for (int i = 0,end=row/2; i < end; i++) {
+            for (int j = 0; j < col; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[row-i-1][j];
+                matrix[row-i-1][j] = temp;
+            }
+        }
+        // 对称翻转
+        for (int i = 0; i < row; i++) {
+            for (int j = i+1; j < col; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
 
 
     public static void main(String[] args) {
