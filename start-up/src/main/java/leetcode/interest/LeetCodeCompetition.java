c527aaea25e3c1e83f4150142adc0aa74eda8bde
kami <zh_guomail@163.com>
2021-04-28 12:44:13 +0800

    [周挑战]Power of Three

--- start-up/src/main/java/leetcode/interest/LeetCodeCompetition.java
+++ start-up/src/main/java/leetcode/interest/LeetCodeCompetition.java
@@ -1458,6 +1458,27 @@ public class LeetCodeCompetition {
         }
         return heights.length - 1;
     }
+    /**
+     * @description: Power of Three
+     * Given an integer n, return true if it is a power of three. Otherwise, return false.
+     * An integer n is a power of three, if there exists an integer x such that n == 3x.
+     * @return: 是否是3的幂次
+     * @author: kami
+     * @关键词：
+     * @date: 2021/4/28 12:36
+     */
+    public boolean isPowerOfThree(int n) {
+        if (n < 1){
+            return false;
+        }
+        while (n > 1){
+            if (n % 3 != 0){
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
         head.next = new ListNode(4);
