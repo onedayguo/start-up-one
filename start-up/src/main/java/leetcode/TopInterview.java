9ae1f00173790c9191caa1d9d580fe18e89b9b2b
kami <zh_guomail@163.com>
2021-05-05 14:43:34 +0800

    [TopInterview]116. Populating Next Right Pointers in Each Node

--- start-up/src/main/java/leetcode/TopInterview.java
+++ start-up/src/main/java/leetcode/TopInterview.java
@@ -339,6 +339,67 @@ public class TopInterview {
         }
         return a;
     }
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+    /**
+     * @description: 116. Populating Next Right Pointers in Each Node
+     * You are given a perfect binary tree where all leaves are on the same level, and every parent has two children.
+     * The binary tree has the following definition:
+     * opulate each next pointer to point to its next right node. If there is no next right node,
+     * the next pointer should be set to NULL.
+     *
+     * Initially, all next pointers are set to NULL.
+     * @return: 填充了next指针的头结点
+     * @author: kami
+     * @关键词： 广度优先遍历
+     * @date: 2021/5/5 14:31
+     */
+    public Node connect(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        if (root == null){
+            return root;
+        }
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            Node pre = queue.poll();
+            if (pre.left != null){
+                queue.add(pre.left);
+            }
+            if (pre.right != null){
+                queue.add(pre.right);
+            }
+            for (int i = 0; i < size-1; i++) {
+                Node cur = queue.poll();;
+                pre.next = cur;;
+                pre = cur;
+                if (cur.left != null){
+                    queue.add(cur.left);
+                }
+                if (cur.right != null){
+                    queue.add(cur.right);
+                }
+            }
+        }
+        return root;
+    }
 
     public static void main(String[] args) {
         int[] nums1 = {0};
