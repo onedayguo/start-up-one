94bc9f10f49c298be4b00d9aefd47d5213d9d924
kami <zh_guomail@163.com>
2021-05-05 09:16:15 +0800

    [周挑战]Non-decreasing Array

--- start-up/src/main/java/leetcode/interest/LeetCodeCompetition.java
+++ start-up/src/main/java/leetcode/interest/LeetCodeCompetition.java
@@ -1671,6 +1671,36 @@ public class LeetCodeCompetition {
         }
         return nums;
     }
+    /**
+     * @description: Non-decreasing Array
+     * Given an array nums with n integers, your task is to check if it could become non-decreasing by
+     * modifying at most one element.
+     * We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based)
+     * such that (0 <= i <= n - 2).
+     * This problem is like a greedy problem. When you find nums[i-1] > nums[i] for some i, you will prefer to
+     * change nums[i-1]'s value, since a larger nums[i] will give you more risks that you get inversion errors after
+     * position i. But, if you also find nums[i-2] > nums[i], then you have to change nums[i]'s value instead,
+     * or else you need to change both of nums[i-2]'s and nums[i-1]'s values.
+     * @return: 是否值修改一个数字就能使数组变为递增的
+     * @author: kami
+     * @关键词： 贪心思想
+     * @date: 2021/5/5 7:56
+     */
+    public boolean checkPossibility(int[] nums) {
+        int cnt = 0;                                                                    //the number of changes
+        for(int i = 1; i < nums.length && cnt<=1 ; i++){
+            // 如果遇到非递增
+            if(nums[i-1] > nums[i]){
+                cnt++;
+                if(i-2<0 || nums[i-2] <= nums[i]) {
+                    nums[i-1] = nums[i];                    //modify nums[i-1] of a priority
+                } else {
+                    nums[i] = nums[i-1];                    //have to modify nums[i]
+                }
+            }
+        }
+        return cnt<=1;
+    }
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
