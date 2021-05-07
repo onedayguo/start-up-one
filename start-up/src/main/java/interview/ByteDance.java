aca20245a875a63fdecdef448d82aea4c6006717
kami <zh_guomail@163.com>
2021-05-07 20:44:06 +0800

    [topinterview] bytedace

--- start-up/src/main/java/interview/ByteDance.java
+++ start-up/src/main/java/interview/ByteDance.java
@@ -1,5 +1,6 @@
 package interview;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
@@ -12,116 +13,117 @@ import java.util.Queue;
  * @Version: 1.0.0
  */
 public class ByteDance {
-/*
-    public static void main(String[] args) {
-        //region 3.0
-//        Scanner in = new Scanner(System.in);
-//        System.out.println("输入正整数N值：");
-//        int N = in.nextInt();
-//        int[] year = new int[N];
-//        for(int i = 0; i < N; i++){
-//            year[i] = in.nextInt();
-//        }
-//        int sum = 0;
-//        int[] money = new int[N];
-//        for (int i = 0; i < N-1; i++) {
-//            if (year[i] >= year[i+1]){
-//                int start = i;
-//                while (year[start] >= year[start+1] && start+1 < N){
-//                    start++;
-//                }
-//                money[start] = 100;
-//                for (int j = start; j > i; j--) {
-//                    if (year[j-1] > year[j]) money[j-1] = money[j]+100;
-//                    if (year[j-1] == year[i]) money[j-1] = money[j];
-//                }
-//            }
-//            if (year[i] < year[i+1]) {
-//                if (i == 0) year[i] = 100;
-//                else year[i] = money[i-1] + 100;
-//            }
-//        }
-//        for (int i = 0; i < N; i++) {
-//            sum += money[i];
-//        }
-//        System.out.println(sum);
-        //endregion
+    /*
+        public static void main(String[] args) {
+            //region 3.0
+    //        Scanner in = new Scanner(System.in);
+    //        System.out.println("输入正整数N值：");
+    //        int N = in.nextInt();
+    //        int[] year = new int[N];
+    //        for(int i = 0; i < N; i++){
+    //            year[i] = in.nextInt();
+    //        }
+    //        int sum = 0;
+    //        int[] money = new int[N];
+    //        for (int i = 0; i < N-1; i++) {
+    //            if (year[i] >= year[i+1]){
+    //                int start = i;
+    //                while (year[start] >= year[start+1] && start+1 < N){
+    //                    start++;
+    //                }
+    //                money[start] = 100;
+    //                for (int j = start; j > i; j--) {
+    //                    if (year[j-1] > year[j]) money[j-1] = money[j]+100;
+    //                    if (year[j-1] == year[i]) money[j-1] = money[j];
+    //                }
+    //            }
+    //            if (year[i] < year[i+1]) {
+    //                if (i == 0) year[i] = 100;
+    //                else year[i] = money[i-1] + 100;
+    //            }
+    //        }
+    //        for (int i = 0; i < N; i++) {
+    //            sum += money[i];
+    //        }
+    //        System.out.println(sum);
+            //endregion
 
 
-         //2.0
-        Scanner in = new Scanner(System.in);
-        System.out.println("输入N和K值：");
-        int N,K;
-        for(int i = 0; i < 1; i++){
-            N = in.nextInt();//长度N
-            K = in.nextInt();//写下K次
+             //2.0
+            Scanner in = new Scanner(System.in);
+            System.out.println("输入N和K值：");
+            int N,K;
+            for(int i = 0; i < 1; i++){
+                N = in.nextInt();//长度N
+                K = in.nextInt();//写下K次
+            }
+            System.out.println("请输入二进制字符串，长度为 N+K-1：");
+            String binaryStr = in.next();//长度 N+K-1
+
+            //region 1.0
+    //        Scanner in = new Scanner(System.in);
+    //        System.out.println("输入N值：");
+    //        int n = in.nextInt();
+    //        System.out.println("输入时间值：");
+    //        int[][] value = new int[n+1][2];
+    //        for(int i = 0; i < n; i++){
+    //            value[i][0] = in.nextInt();// 0<= value <24
+    //            value[i][1] = in.nextInt();//0<= value <60
+    //        }
+    //        int X = in.nextInt(); //到教室需要时间  0<=X<=100
+    //        for (int i = 0; i < 1; i++) {
+    //            value[n][0] = in.nextInt();// 0<= value <24
+    //            value[n][1] = in.nextInt();//0<= value <60
+    //        }
+    //
+    //        int maxhour = 0;
+    //        int maxMinu = 0;
+    //        for (int i = 0; i < n; i++) {
+    //            int iHour = value[i][0],iMinu =value[i][1];
+    //            if (iMinu + X >= 60){
+    //                iHour += 1;
+    //                iMinu %= 60;
+    //            }
+    //            if ((iHour == value[n][0] && iMinu<= value[n][1]) || iHour < value[n][0]){
+    //                if (value[i][0] > maxhour){
+    //                    maxhour = value[i][0];
+    //                    maxMinu = value[i][1];
+    //                }else if (value[i][0] == maxhour){
+    //                    if (value[i][1] > maxMinu) maxMinu = value[i][1];
+    //                }
+    //            }
+    //        }
+    //        System.out.println(maxhour+" "+maxMinu);
+            //endregion
+
         }
-        System.out.println("请输入二进制字符串，长度为 N+K-1：");
-        String binaryStr = in.next();//长度 N+K-1
 
-        //region 1.0
-//        Scanner in = new Scanner(System.in);
-//        System.out.println("输入N值：");
-//        int n = in.nextInt();
-//        System.out.println("输入时间值：");
-//        int[][] value = new int[n+1][2];
-//        for(int i = 0; i < n; i++){
-//            value[i][0] = in.nextInt();// 0<= value <24
-//            value[i][1] = in.nextInt();//0<= value <60
-//        }
-//        int X = in.nextInt(); //到教室需要时间  0<=X<=100
-//        for (int i = 0; i < 1; i++) {
-//            value[n][0] = in.nextInt();// 0<= value <24
-//            value[n][1] = in.nextInt();//0<= value <60
-//        }
-//
-//        int maxhour = 0;
-//        int maxMinu = 0;
-//        for (int i = 0; i < n; i++) {
-//            int iHour = value[i][0],iMinu =value[i][1];
-//            if (iMinu + X >= 60){
-//                iHour += 1;
-//                iMinu %= 60;
-//            }
-//            if ((iHour == value[n][0] && iMinu<= value[n][1]) || iHour < value[n][0]){
-//                if (value[i][0] > maxhour){
-//                    maxhour = value[i][0];
-//                    maxMinu = value[i][1];
-//                }else if (value[i][0] == maxhour){
-//                    if (value[i][1] > maxMinu) maxMinu = value[i][1];
-//                }
-//            }
-//        }
-//        System.out.println(maxhour+" "+maxMinu);
-        //endregion
-
-    }
-
-*/
-    private int getMaxOnlineUsers(List<UserLoginRecord> records){
+    */
+    private int getMaxOnlineUsers(List<UserLoginRecord> records) {
         Calendar cal = Calendar.getInstance();
         cal.set(Calendar.HOUR_OF_DAY, 0);
         cal.set(Calendar.SECOND, 0);
         cal.set(Calendar.MINUTE, 0);
         cal.set(Calendar.MILLISECOND, 0);
-        Long startTimeDay = cal.getTimeInMillis()/1000;
+        Long startTimeDay = cal.getTimeInMillis() / 1000;
         // 登入登出记录
         int[] logRecord = new int[86400];
         // 每一秒钟的在线人数
         int[] onlineUserRecord = new int[86400];
-        for (UserLoginRecord record: records) {
-            logRecord[(int)(record.getLoginTime()-startTimeDay)]+=1;
-            logRecord[(int)(record.getLogoutTime()-startTimeDay)]-=1;
+        for (UserLoginRecord record : records) {
+            logRecord[(int) (record.getLoginTime() - startTimeDay)] += 1;
+            logRecord[(int) (record.getLogoutTime() - startTimeDay)] -= 1;
         }
         onlineUserRecord[0] = logRecord[0];
         int maxOnline = 0;
         for (int i = 1; i < 86400; i++) {
-            onlineUserRecord[i] = onlineUserRecord[i-1]+logRecord[i];
-            maxOnline = Math.max(maxOnline,onlineUserRecord[i]);
+            onlineUserRecord[i] = onlineUserRecord[i - 1] + logRecord[i];
+            maxOnline = Math.max(maxOnline, onlineUserRecord[i]);
         }
         return maxOnline;
     }
-    static class UserLoginRecord{
+
+    static class UserLoginRecord {
         Long userId;
         Long loginTime;
         Long logoutTime;
@@ -157,7 +159,8 @@ public class ByteDance {
         public Node right;
         public Node next;
 
-        public Node() {}
+        public Node() {
+        }
 
         public Node(int _val) {
             val = _val;
@@ -174,12 +177,12 @@ public class ByteDance {
     /**
      * @description: 117. Populating Next Right Pointers in Each Node II
      * Given a binary tree
-     *
+     * <p>
      * struct Node {
-     *   int val;
-     *   Node *left;
-     *   Node *right;
-     *   Node *next;
+     * int val;
+     * Node *left;
+     * Node *right;
+     * Node *next;
      * }
      * Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
      * Initially, all next pointers are set to NULL.
@@ -221,22 +224,36 @@ public class ByteDance {
         return root;
     }
 
+    /**
+     * @description: 字节面试，查找所有的组合
+     * @return: 组合列表
+     * @author: kami
+     * @关键词： 全排列
+     * @date: 2021/5/7 18:01
+     */
+    public List<List<Integer>> findAllCombination(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+        res.add(new LinkedList<>());
+        backtrack(res, nums, 0, new ArrayList<>());
+        return res;
+    }
+
+    private void backtrack(List<List<Integer>> res, int[] nums, int index, List<Integer> cur) {
+        if (cur.size() > 0) {
+            res.add(new ArrayList<>(cur));
+        }
+        for (int i = index; i < nums.length; i++) {
+            cur.add(nums[i]);
+            backtrack(res, nums, i + 1, cur);
+            cur.remove(cur.size() - 1);
+        }
+    }
+
     public static void main(String[] args) {
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.HOUR_OF_DAY, 0);
-        cal.set(Calendar.SECOND, 0);
-        cal.set(Calendar.MINUTE, 0);
-        cal.set(Calendar.MILLISECOND, 0);
-        Long startTime = cal.getTimeInMillis()/1000;
-        System.out.println(startTime);
-        cal.set(Calendar.HOUR_OF_DAY, 24);
-        cal.set(Calendar.SECOND, 0);
-        cal.set(Calendar.MINUTE, 0);
-        cal.set(Calendar.MILLISECOND, 0);
-        Long endTime = cal.getTimeInMillis()/1000;
-        System.out.println(endTime);
-        Long diffTime = endTime-startTime;
-        System.out.println("endTime-startTime="+diffTime);
+        ByteDance b = new ByteDance();
+        int[] nu = {1, 2, 2};
+        List<List<Integer>> allCombination = b.findAllCombination(nu);
+        System.out.println("");
     }
 
 }
--- start-up/src/main/java/leetcode/TopInterview.java
+++ start-up/src/main/java/leetcode/TopInterview.java
@@ -530,6 +530,23 @@ public class TopInterview {
         }
         return length;
     }
+    /**
+     * @description: 210. Course Schedule II
+     * There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an
+     * array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want
+     * to take course ai.
+     *
+     * For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+     * Return the ordering of courses you should take to finish all courses. If there are many valid answers,
+     * return any of them. If it is impossible to finish all courses, return an empty array.
+     * @return: 上课的顺序
+     * @author: kami
+     * @关键词：
+     * @date: 2021/5/7 7:37
+     */
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        return null;
+    }
 
     public static void main(String[] args) {
         int[] nums1 = {0};
