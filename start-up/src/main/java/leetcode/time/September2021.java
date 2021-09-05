ccc77c337c2664f08a1ac35773daee277cf2a639
kami <zh_guomail@163.com>
2021-09-05 22:40:22 +0800

    [9月-数组]154. Find Minimum in Rotated Sorted Array II

--- start-up/src/main/java/leetcode/time/September2021.java
+++ start-up/src/main/java/leetcode/time/September2021.java
@@ -21,23 +21,24 @@ public class September2021 {
      * @date: 2021/9/1 8:32
      */
     public int findMin(int[] nums) {
-        if (nums.length == 1){
+        if (nums.length == 1) {
             return nums[0];
         }
-        int left = 0,right = nums.length-1;
-        while (left < right){
-            if (nums[left] < nums[right]){
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] < nums[right]) {
                 return nums[left];
             }
-            int mid = (left+right)/2;
-            if (nums[mid] > nums[right]){
-                left = mid+1;
-            }else {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
                 right = mid;
             }
         }
         return nums[left];
     }
+
     /**
      * @description: 137. Single Number II
      * Given an integer array nums where every element appears three times except for one, which appears
@@ -50,7 +51,7 @@ public class September2021 {
      */
     public int singleNumber(int[] nums) {
         int ans = 0;
-        for(int i = 0; i < 32; i++) {
+        for (int i = 0; i < 32; i++) {
             int sum = 0;
             for (int num : nums) {
                 if (((num >> i) & 1) == 1) {
@@ -58,10 +59,47 @@ public class September2021 {
                     sum %= 3;
                 }
             }
-            if(sum != 0) {
+            if (sum != 0) {
                 ans |= sum << i;
             }
         }
         return ans;
+    }
+
+    /**
+     * @description: 154. Find Minimum in Rotated Sorted Array II
+     * Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example,
+     * the array nums = [0,1,4,4,5,6,7] might become:
+     * <p>
+     * [4,5,6,7,0,1,4] if it was rotated 4 times.
+     * [0,1,4,4,5,6,7] if it was rotated 7 times.
+     * Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1],
+     * a[2], ..., a[n-2]].
+     * <p>
+     * Given the sorted rotated array nums that may contain duplicates, return the minimum element of this array.
+     * <p>
+     * You must decrease the overall operation steps as much as possible.
+     * @return: 有序数组中的最小值
+     * @author: kami
+     * @关键词： 非递减的序列，可能有重复元素，二分查找
+     * @date: 2021/9/5 10:24
+     */
+    public int findMin1(int[] nums) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        int mid = 0;
+
+        while (lo < hi) {
+            mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] > nums[hi]) {
+                lo = mid + 1;
+            } else if (nums[mid] < nums[hi]) {
+                hi = mid;
+            } else { // when num[mid] and num[hi] are same
+                hi--;
+            }
+        }
+        return nums[lo];
     }
 }
