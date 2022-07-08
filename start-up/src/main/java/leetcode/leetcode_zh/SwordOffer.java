38e5cf6b50f69f9ccd631c217ffad64650b79e2e
kami <zh_guomail@163.com>
2022-07-08 21:18:00 +0800

    剑指 Offer 36. 二叉搜索树与双向链表

--- start-up/src/main/java/leetcode/leetcode_zh/SwordOffer.java
+++ start-up/src/main/java/leetcode/leetcode_zh/SwordOffer.java
@@ -1,5 +1,10 @@
 package leetcode.leetcode_zh;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class SwordOffer {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null)
@@ -11,5 +16,48 @@ public class SwordOffer {
             return lowestCommonAncestor(root.right, p, q);
 
         return root;
+    }
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    }
+
+    Node pre, head;
+
+    /**
+     * 剑指 Offer 36. 二叉搜索树与双向链表
+     * @param root
+     * @return
+     */
+    public Node treeToDoublyList(Node root) {
+        if(root == null) return null;
+        dfs(root);
+        head.left = pre;
+        pre.right = head;
+        return head;
+    }
+    void dfs(Node cur) {
+        if(cur == null) return;
+        dfs(cur.left);
+        if(pre != null) pre.right = cur;
+        else head = cur;
+        cur.left = pre;
+        pre = cur;
+        dfs(cur.right);
     }
 }
